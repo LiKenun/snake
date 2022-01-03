2b78aface2cb7eccdaf391effbcd999da5555bf9
Kevin Li <lku@kenun.li>
2022-01-03 16:46:28 -0500

    Initial commit

---
+++ src/main/java/Direction.java
@@ -0,0 +1,13 @@
+/*
+ * CST 3513 Final Project: Snake Game
+ * Group 1 (Asra Pervaiz, Kevin Li, Naheed Reyhad, Martin Liang) @6/17/2021
+ *
+ * A simple enumeration of all the possible directions a snake can move.
+ */
+
+public enum Direction {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+}
---
+++ src/main/java/EntityType.java
@@ -0,0 +1,12 @@
+/*
+ * CST 3513 Final Project: Snake Game
+ * Group 1 (Asra Pervaiz, Kevin Li, Naheed Reyhad, Martin Liang) @6/17/2021
+ *
+ * An enumeration of all the kinds of entities that can exist within a game.
+ */
+
+enum EntityType {
+    WALL,
+    SNAKE,
+    APPLE
+}
